8b94ad748314e8874e2d53cfc2f679a205255a30
jkazuo55 <jkazuo555@gmail.com>
2019-05-10 01:38:26 +0000

    corrigiendo relaciones

--- app/Tarea.php
+++ app/Tarea.php
@@ -17,7 +17,7 @@ class Tarea extends Model
      * Una tarea pertenece a un usuario
      */
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo('App\User','user_id');
     }
     /**
      * Una tarea tiene uno a muchos archivos de tareas
